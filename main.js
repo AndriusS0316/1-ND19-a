285d6598ee09c396021b0a31a874927fb74606c4
Andrius <andriuss0316@gmail.com>
2021-08-14 19:54:53 +0300

    20210814

--- main.js
+++ main.js
@@ -2,14 +2,3 @@
 
 
 
-// PAPILDOMI KOMENTARAI:
-// Iprastinio if panaudojimas pasirenkant langelius
-
-if (r % 2 === 0) {
-  cellHTML = whiteBlackCellHTML;
-} else {
-  cellHTML = blackWhiteCellHTML;
-}
-// Ternary operatoriaus panaudojimas pasirenkant langelius
-
-cellHTML = r % 2 === 0 ? whiteBlackCellHTML : blackWhiteCellHTML;
